3034b163da1a4d2a50da6caf19dec613ca4f46b0
mainul35 <mainuls18@gmail.com>
2022-10-11 20:48:03 +0700

    validation endpoint returning boolean

--- bs-auth/bs-auth-implementation/src/main/java/com/mainul35/auth/controllers/TokenController.java
+++ bs-auth/bs-auth-implementation/src/main/java/com/mainul35/auth/controllers/TokenController.java
@@ -2,6 +2,7 @@ package com.mainul35.auth.controllers;
 
 import com.mainul35.auth.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,12 @@ public class TokenController {
     private TokenService tokenService;
 
     @PostMapping("/validate")
-    public void validateToken(HttpServletRequest request) throws Exception {
+    public ResponseEntity<Boolean> validateToken(HttpServletRequest request) throws Exception {
         String jwtToken = "";
         String authHeader = request.getHeader(AUTHORIZATION);
         if (!authHeader.isEmpty()) {
             jwtToken = authHeader.split("\\s")[1];
         }
-        tokenService.validateToken(jwtToken);
+        return ResponseEntity.ok(tokenService.validateToken(jwtToken));
     }
 }
