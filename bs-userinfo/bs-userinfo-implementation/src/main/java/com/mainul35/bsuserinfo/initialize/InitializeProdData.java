1f4d85120fc9e3eef02b4fced1428f348a1caba1
mainul35 <mainuls18@gmail.com>
2022-10-23 19:13:57 +0700

    app-social-medium - production profile configuration file updated

--- bs-userinfo/bs-userinfo-implementation/src/main/java/com/mainul35/bsuserinfo/initialize/InitializeProdData.java
+++ bs-userinfo/bs-userinfo-implementation/src/main/java/com/mainul35/bsuserinfo/initialize/InitializeProdData.java
@@ -1,87 +1,39 @@
-//package com.mainul35.bsuserinfo.initialize;
-//
-//import com.fasterxml.jackson.core.type.TypeReference;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.traver.authserver.models.RoleModel;
-//import com.traver.authserver.models.UserModel;
-//import com.traver.authserver.repositories.RoleRepository;
-//import com.traver.authserver.repositories.UserRepository;
-//import com.traver.authserver.services.TokenService;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.core.io.ResourceLoader;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Profile("prod")
-//@Component
-//public class InitializeProdData implements InitializeData {
-//
-//    private final UserRepository userRepository;
-//
-//    private final RoleRepository roleRepository;
-//
-//    private final PasswordEncoder passwordEncoder;
-//
-//    private final TokenService tokenService;
-//
-//    private final ResourceLoader resourceLoader;
-//
-//    public InitializeProdData(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, TokenService tokenService, ResourceLoader resourceLoader) {
-//        this.userRepository = userRepository;
-//        this.roleRepository = roleRepository;
-//        this.passwordEncoder = passwordEncoder;
-//        this.tokenService = tokenService;
-//        this.resourceLoader = resourceLoader;
-//    }
-//
-//    @Override
-//    public void initialize() {
-//        addRoles ();
-//        addUsers ();
-//    }
-//
-//    private void addUsers() {
-//        try {
-//            List<UserModel> UserModels = new ObjectMapper ()
-//                    .readValue (
-//                            resourceLoader.getResource ("classpath:users.json").getInputStream (),
-//                            new TypeReference<ArrayList<UserModel>> () {
-//                            }
-//                    );
-//            UserModels.forEach (UserModel -> {
-//                boolean found = userRepository
-//                        .findByUsernameOrEmail (UserModel.getUsername (), UserModel.getEmail ())
-//                        .size () > 0;
-//                if (!found) {
-//                    tokenService.generateToken (UserModel);
-//                    UserModel.setPassword (passwordEncoder.encode (UserModel.getPassword ()));
-//                    userRepository.saveAndFlush (UserModel);
-//                }
-//            });
-//        } catch (IOException e) {
-//            e.printStackTrace ();
-//        }
-//    }
-//
-//    private void addRoles() {
-//        try {
-//            List<RoleModel> RoleModels = new ObjectMapper ()
-//                    .readValue (
-//                            resourceLoader.getResource ("classpath:roles.json").getInputStream (),
-//                            new TypeReference<ArrayList<RoleModel>> () {
-//                            }
-//                    );
-//            RoleModels.forEach (RoleModel -> {
-//                if (!roleRepository.findByRole (RoleModel.getRole ()).isPresent ()) {
-//                    roleRepository.saveAndFlush (RoleModel);
-//                }
-//            });
-//        } catch (IOException e) {
-//            e.printStackTrace ();
-//        }
-//    }
-//}
+package com.mainul35.bsuserinfo.initialize;
+
+import com.mainul35.bsuserinfo.repositories.UserInfoRepository;
+import com.mainul35.bsuserinfo.services.impl.UserInfoServiceImpl;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Profile({"prod"})
+@Component
+public class InitializeProdData implements InitializeData {
+
+    private final UserInfoRepository userRepository;
+
+    private final UserInfoServiceImpl userInfoService;
+
+    private final ResourceLoader resourceLoader;
+
+    public InitializeProdData(UserInfoRepository userRepository, UserInfoServiceImpl userInfoService, ResourceLoader resourceLoader) {
+        this.userRepository = userRepository;
+        this.userInfoService = userInfoService;
+        this.resourceLoader = resourceLoader;
+    }
+
+    @Override
+    public void initialize() {
+        addUsers ();
+    }
+
+    @Override
+    public void doCleanUp() {
+        userRepository.deleteAll ();
+    }
+
+    private void addUsers() {
+
+        InitializeTestData.populateUsers(userRepository, resourceLoader, userInfoService);
+    }
+}
